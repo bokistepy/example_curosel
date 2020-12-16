0bde4ff3436d496dcfc1db86822d8ecc98af79e5
Boban Jovanoski <bobanj@gmail.com>
2020-12-16 10:54:06 +0100

    Update carusel.js
    Should work, caching containers should speed things up, haven't tested it XD

--- carousel.js
+++ carousel.js
@@ -1,14 +1,16 @@
-var startingItem = 3;
+let startingItem = 3;
 
 $(document).ready(function() {
+	let carusel = $('#carousel');
+	let captions = $('#captions')
 	$('.carousel_data .carousel_item').each(function(){
-		$('#carousel').append( $(this).find('.image').html() );
+		carusel.append( $(this).find('.image').html() );
 	});
-	createCarousel();
-	showCaption();
+	createCarousel(carusel, captions);
+	showCaption(carusel, captions);
 });
 
-function createCarousel(){
+function createCarousel(carusel, captions){
 	$('div#carousel').roundabout({
 		startingChild: window.startingItem,
 		childSelector: 'img',
@@ -19,37 +21,37 @@ function createCarousel(){
 		clickToFocus: true,
 		clickToFocusCallback: showCaption
 	});
-	createCustomButtons();
+	createCustomButtons(carusel, captions);
 }
 
-function createCustomButtons(){
+function createCustomButtons(carusel, captions){
 
 	$('.nextItem').click(function(){
-		hideCaption();
-		$('div#carousel').roundabout('animateToNextChild', showCaption);
+		hideCaption(captions);
+		carusel.roundabout('animateToNextChild', showCaption);
 	});
 
 	$('.prevItem').click(function(){
-		hideCaption();
-		$('div#carousel').roundabout('animateToPreviousChild', showCaption);
+		hideCaption(captions);
+		carusel.roundabout('animateToPreviousChild', showCaption);
 	});
 
 	$('div#carousel img').click(function(){
-		hideCaption();
+		hideCaption(captions);
 	});
 }
 
-function hideCaption(){
-	$('#captions').animate({'opacity':0}, 250);
+function hideCaption(captions){
+	captions.animate({'opacity':0}, 250);
 }
 
-function showCaption(){
-	var childInFocus = $('div#carousel').data('roundabout').childInFocus
-	var setCaption = $('.carousel_data .carousel_item .caption:eq('+childInFocus+')').html();
-	$('#captions').html(setCaption);
-	var newHeight = $('#captions').height()+'px';
-	$('.caption_container').animate({'height':newHeight}, 500, function(){
-		$('#captions').animate({'opacity':1}, 250);
+function showCaption(carusel, captions){
+	var childInFocus = carusel.data('roundabout').childInFocus
+	var captionHtml = $('.carousel_data .carousel_item .caption:eq('+childInFocus+')').html();
+	captions.html(captionHtml);
+	var newHeight = captions.height()+'px';
+	carusel.parent().animate({'height':newHeight}, 500, function(){
+		captions.animate({'opacity':1}, 250);
 	});
 
 }
